c65a2073815d5e18bf045953ce76970ad7be00dd
Amr Khaled <amrkhaled.m27@gmail.com>
2023-08-28 19:26:10 +0300

    add images and data to the projects pages and create a footer

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
-// const lenis = new Lenis();
+const lenis = new Lenis();
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
 
-// lenis.on("scroll", (e) => {
-//   console.log(e);
+requestAnimationFrame(raf);
+const swup = new Swup({
+  doScrollingRightAway: true,
+});
+
+swup.on("animationInDone", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth", // Add smooth scrolling behavior
+  });
+});
+// window.addEventListener("popstate", () => {
+//   // Manually trigger the animation when using browser navigation
+//   swup.plugins.doScrollingRightAway.setOriginalData(null);
+//   swup.doScrolling(swup.options.scrollPageDuration).then(() => {
+//     swup.triggerPage();
+//   });
 // });
-
-// function raf(time) {
-//   lenis.raf(time);
-//   requestAnimationFrame(raf);
-// }
-
-// requestAnimationFrame(raf);
-const swup = new Swup();
+// const scrollToSection = (section) => {
+//   window.scrollTo({
+//     top: document.querySelector(section).offsetTop,
+//     behavior: "smooth",
+//   });
+// };
+// document.querySelectorAll(".linksList a").forEach((link) => {
+//   link.addEventListener("click", function (e) {
+//     if (window.location.pathname !== "/home.html") {
+//       window.location.href = link.getAttribute("href");
+//       // window.onload = () => {
+//       //   console.log("loaded");
+//       //   scrollToSection(link.getAttribute("href"));
+//       // };
+//     }
+//     scrollToSection(link.getAttribute("href"));
+//   });
+// });
